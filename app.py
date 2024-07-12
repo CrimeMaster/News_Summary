9cb7d448b604754d94cbadcc6cd0c5e37325d5b7
CrimeMaster <gtirkey300@gmail.com>
2024-07-12 21:04:34 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from summary_page import show_summary_page
 
-page = st.sidebar.selectbox("Summary and Analyse", ("Summary"))
+
 
 
 st.title("News Summary")
 
-if page == "Summary":
-    show_summary_page()
+show_summary_page()
